83631235024af13ab6d24b2e873319d187efa45a
SantiagoAlbi <84410098+SantiagoAlbi@users.noreply.github.com>
2024-06-17 20:37:58 -0300

    Add files via upload

---
+++ 1-project1/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">=1.7.0, <2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    random = {
+
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
